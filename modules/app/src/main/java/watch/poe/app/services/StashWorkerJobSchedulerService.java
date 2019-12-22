707f12b7effec2ee3d789d635e00ee1ec6ba43d0
siegrest <179900@ttu.ee>
2019-12-22 12:09:00 +0200

    Clean up StashWorkerManagerService and StashWorkerJobSchedulerService

--- modules/app/src/main/java/watch/poe/app/services/StashWorkerJobSchedulerService.java
+++ modules/app/src/main/java/watch/poe/app/services/StashWorkerJobSchedulerService.java
@@ -1,18 +1,18 @@
 package watch.poe.app.services;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import watch.poe.app.config.AppModuleConfig;
 import watch.poe.app.utility.ChangeIdUtility;
 
 @Slf4j
 @Service
 public class StashWorkerJobSchedulerService {
 
-    @Autowired
-    private AppModuleConfig config;
+    @Value("${stash.fetch.rate}")
+    private int fetchRate;
 
+    // todo: query job from repository
     private String job = "0-0-0-0-0";
     private long lastPollTime = 0;
 
@@ -36,7 +36,6 @@ public class StashWorkerJobSchedulerService {
             log.error("Change id {} was overwritten by {}", job, newJob);
         }
 
-        log.info("found new job {}", newJob); // todo: remove me
         job = newJob;
     }
 
@@ -49,7 +48,7 @@ public class StashWorkerJobSchedulerService {
     }
 
     public boolean isCooldown() {
-        return System.currentTimeMillis() - lastPollTime < config.getPropertyAsInt("stash.fetch.rate");
+        return System.currentTimeMillis() - lastPollTime < fetchRate;
     }
 
 }
--- modules/app/src/main/java/watch/poe/app/services/StashWorkerManagerService.java
+++ modules/app/src/main/java/watch/poe/app/services/StashWorkerManagerService.java
@@ -2,9 +2,9 @@ package watch.poe.app.services;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import watch.poe.app.config.AppModuleConfig;
 
 import java.util.LinkedList;
 import java.util.concurrent.Future;
@@ -19,51 +19,37 @@ public class StashWorkerManagerService {
     @Autowired
     private StashWorkerJobSchedulerService jobSchedulerService;
 
-    @Autowired
-    private AppModuleConfig config;
+    @Value("${stash.worker.count}")
+    private int maxWorkerCount;
 
     private LinkedList<Future<String>> workerResultQueue = new LinkedList<>();
 
-    @Scheduled(fixedRate = 1000)
+    @Scheduled(fixedRate = 100)
     public void scheduleWorker() {
+        workerResultQueue.removeIf(Future::isDone);
+
         if (workerResultQueue.isEmpty() && jobSchedulerService.isJobEmpty()) {
-            throw new RuntimeException("No active workers and next change id is null");
+            log.error("No active workers and next change id is empty");
+            return;
         }
 
-        if (hasCompletedWorkers()) {
-            log.info("found completed workers. processing...");
-            workerResultQueue.removeIf(Future::isDone);
-        }
-
-        // no available job
         if (jobSchedulerService.isJobEmpty()) {
             return;
         }
 
-        // worker limit reached
-        if (workerResultQueue.size() >= config.getPropertyAsInt("stash.worker.count")) {
-            log.info("worker limit reached");
+        if (workerResultQueue.size() >= maxWorkerCount) {
+            log.debug("Worker limit reached");
             return;
         }
 
         if (jobSchedulerService.isCooldown()) {
-            log.info("poll limit reached");
+            log.debug("Poll on cooldown");
             return;
         }
 
         jobSchedulerService.bumpPollTime();
         var result = stashWorkerService.queryNext();
         workerResultQueue.push(result);
-    }
-
-    private boolean hasCompletedWorkers() {
-        for (var workerResult : workerResultQueue) {
-            if (workerResult.isDone()) {
-                return true;
-            }
-        }
-
-        return false;
     }
 
 }
