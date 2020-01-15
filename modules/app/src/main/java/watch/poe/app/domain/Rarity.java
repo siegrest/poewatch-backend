aed88a194654b494e98a350f420ea0b167da2cb0
siegrest <179900@ttu.ee>
2020-01-15 23:19:57 +0200

    Fix int not deserialized to enum

--- modules/app/src/main/java/watch/poe/app/domain/Rarity.java
+++ modules/app/src/main/java/watch/poe/app/domain/Rarity.java
@@ -1,14 +1,27 @@
 package watch.poe.app.domain;
 
+import com.google.gson.annotations.SerializedName;
+
 public enum Rarity {
+  // todo: fix this clusterfuck
+  @SerializedName("0")
   Normal,
+  @SerializedName("1")
   Magic,
+  @SerializedName("2")
   Rare,
+  @SerializedName("3")
   Unique,
+  @SerializedName("4")
   Gem,
+  @SerializedName("5")
   Currency,
+  @SerializedName("6")
   Div,
+  @SerializedName("7")
   Quest,
+  @SerializedName("8")
   Prophecy,
+  @SerializedName("9")
   Relic
 }
