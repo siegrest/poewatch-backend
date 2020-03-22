4168977f3b30a484604d9880e853f5d7b1e24269
siegrest <179900@ttu.ee>
2020-03-22 19:09:59 +0200

    Fix missing hibernate sequence

--- database/init/createdb.sh
+++ database/init/createdb.sh
@@ -2,4 +2,4 @@
 
 psql -d "postgres" -U "postgres" -w -f /docker-entrypoint-initdb.d/db_create_user.psql
 psql -d "postgres" -U "postgres" -w -f /docker-entrypoint-initdb.d/db_create_database.psql
-psql -d "pw" -U "pw" -w -f /docker-entrypoint-initdb.d/db_init_database.psql
+psql -d "pw" -U "postgres" -w -f /docker-entrypoint-initdb.d/db_init_database.psql
