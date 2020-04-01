679865bf4427826376af7d158c665fc95ffcabe2
siegrest <179900@ttu.ee>
2020-04-01 13:29:13 +0300

    Fix league dates not saved

--- modules/app/src/main/java/watch/poe/app/utility/DateUtility.java
+++ modules/app/src/main/java/watch/poe/app/utility/DateUtility.java
@@ -1,13 +1,16 @@
 package watch.poe.app.utility;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
 
+@Slf4j
 public final class DateUtility {
 
-    public static SimpleDateFormat isoSdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
+    public static SimpleDateFormat isoSdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
 
     static {
         isoSdf.setTimeZone(TimeZone.getTimeZone("UTC"));
@@ -21,6 +24,7 @@ public final class DateUtility {
         try {
             return isoSdf.parse(date);
         } catch (ParseException ex) {
+            log.error("Could no parse iso date", ex);
             return null;
         }
     }
